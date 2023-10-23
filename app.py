2d574055d66652632d2dd9154661965373cb74fa
Yinyi <33323282+yinyifeng@users.noreply.github.com>
2023-10-24 00:55:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.datasets import load_diabetes
+from sklearn.datasets import fetch_california_housing
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn import metrics as mt
@@ -13,187 +15,275 @@ from PIL import Image
 import altair as alt
 from htbuilder import HtmlElement, div, hr, a, p, img, styles
 from htbuilder.units import percent, px
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
+
+
+
+
+
+
+data_url = "https://www.kaggle.com/datasets/rajatkumar30/food-delivery-time" 
+
+# setting up the page streamlit
+
+st.set_page_config(
+    page_title="Streamlit Food Delivery Time App", layout="wide", page_icon="./images/linear-regression.png"
+)
+
 
 def main():
-    image_nyu = Image.open('nyu.png')
-    st.image(image_nyu, width=100)
-    
-    st.title("Food Delivery Times 🍜")
-    
-    # navigation dropdown
-    
-    st.sidebar.header("Dashboard")
-    st.sidebar.markdown("---")
-    app_mode = st.sidebar.selectbox('🔎 Select Page',['Introduction','Visualization','Prediction'])
-    
-    # Dropdown menu for selecting the dataset (currently only "Wine Quality" is available)
-    select_dataset =  st.sidebar.selectbox('💾 Select Dataset',["Food Delivery Times"])
-    
-    # Load the wine quality dataset
-    df = pd.read_csv("deliverytime.csv")
-    
-    # Dropdown menu for selecting which variable from the dataset to predict
-    list_variables = df.columns
-    select_variable =  st.sidebar.selectbox('🎯 Select Variable to Predict',list_variables)
-    
-    # Introduction page content
-    if app_mode == 'Introduction':
-        # Display dataset details
-        st.markdown("### 00 - Show Dataset")
-    
-        # Split the page into 10 columns to display information about each wine quality variable
-        col1, col2, col3, col4, col5, col6, col7, col8, col9, col10 = st.columns(10)
-    
-        # Descriptions for each variable in the dataset
-        # ... [The code here provides descriptions for each wine quality variable]
-    
-        # Allow users to view either the top or bottom rows of the dataset
-        num = st.number_input('No. of Rows', 5, 10)
-        head = st.radio('View from top (head) or bottom (tail)', ('Head', 'Tail'))
-        if head == 'Head':
-            st.dataframe(df.head(num))
-        else:
-            st.dataframe(df.tail(num))
-    
-        # Display the shape (number of rows and columns) of the dataset
-        st.markdown("Number of rows and columns helps us to determine how large the dataset is.")
-        st.text('(Rows,Columns)')
-        st.write(df.shape)
-    
-        st.markdown("### 01 - Description")
-        st.dataframe(df.describe())
-    
-    
-    
-        st.markdown("### 02 - Missing Values")
-        st.markdown("Missing values are known as null or NaN values. Missing data tends to **introduce bias that leads to misleading results.**")
-        dfnull = df.isnull().sum()/len(df)*100
-        totalmiss = dfnull.sum().round(2)
-        st.write("Percentage of total missing values:",totalmiss)
-        st.write(dfnull)
-        if totalmiss <= 30:
-            st.success("Looks good! as we have less then 30 percent of missing values.")
-        else:
-            st.warning("Poor data quality due to greater than 30 percent of missing value.")
-            st.markdown(" > Theoretically, 25 to 30 percent is the maximum missing values are allowed, there's no hard and fast rule to decide this threshold. It can vary from problem to problem.")
-    
-        st.markdown("### 03 - Completeness")
-        st.markdown(" Completeness is defined as the ratio of non-missing values to total records in dataset.")
-        # st.write("Total data length:", len(df))
-        nonmissing = (df.notnull().sum().round(2))
-        completeness= round(sum(nonmissing)/len(df),2)
-        st.write("Completeness ratio:",completeness)
-        st.write(nonmissing)
-        if completeness >= 0.80:
-            st.success("Looks good! as we have completeness ratio greater than 0.85.")
-    
-        else:
-            st.success("Poor data quality due to low completeness ratio( less than 0.85).")
-    
-    if app_mode == 'Visualization':
-        # Display a header for the Visualization section
-        st.markdown("## Visualization")
-    
-        # Allow users to select two variables from the dataset for visualization
-        symbols = st.multiselect("Select two variables", list_variables, ["Type_of_vehicle", "Time_taken(min)"])
-    
-        # Create a slider in the sidebar for users to adjust the plot width
-        width1 = st.sidebar.slider("plot width", 1, 25, 10)
-    
-        # Create tabs for different types of visualizations
-        tab1, tab2 = st.tabs(["Line Chart", "📈 Correlation"])
-    
-        # Content for the "Line Chart" tab
-        tab1.subheader("Line Chart")
-        # Display a line chart for the selected variables
-        st.line_chart(data=df, x=symbols[0], y=symbols[1], width=0, height=0, use_container_width=True)
-        # Display a bar chart for the selected variables
-        st.bar_chart(data=df, x=symbols[0], y=symbols[1], use_container_width=True)
-    
-        # Content for the "Correlation" tab
-        tab2.subheader("Correlation Tab 📉")
-        # Create a heatmap to show correlations between variables in the dataset
-        fig, ax = plt.subplots(figsize=(width1, width1))
-        sns.heatmap(df.corr(), cmap=sns.cubehelix_palette(8), annot=True, ax=ax)
-        tab2.write(fig)
-    
-        # Display a pairplot for the first five variables in the dataset
-        st.markdown("### Pairplot")
-        df2 = df
-    
-        fig3 = sns.pairplot(df2)
-        st.pyplot(fig3)
-    
-    # Check if the app mode is set to 'Prediction'
-    if app_mode == 'Prediction':
-        # Display a header for the Prediction section
-        st.markdown("## Prediction")
-    
-        # Allow users to adjust the size of the training dataset using a slider in the sidebar
-        train_size = st.sidebar.number_input("Train Set Size", min_value=0.00, step=0.01, max_value=1.00, value=0.70)
-    
-        # Drop the selected variable from the dataset to prepare for prediction
-        new_df = df.drop(labels=select_variable, axis=1)
-        list_var = new_df.columns
-    
-        # Allow users to select explanatory variables for prediction
-        output_multi = st.multiselect("Select Explanatory Variables", list_var)
+    def _max_width_():
+        max_width_str = f"max-width: 1000px;"
+        st.markdown(
+            f"""
+        <style>
+        .reportview-container .main .block-container{{
+            {max_width_str}
+        }}
+        </style>
+        """,
+            unsafe_allow_html=True,
+        )
 
-        # Call the prediction function and store the results
-        X_train, X_test, y_train, y_test, predictions, x, y = prediction(select_variable, train_size, new_df, list_var)
-        
-        # Display the results header in the Streamlit app
-        st.subheader('🎯 Results')
-        
-        # Display various metrics about the prediction results in the Streamlit app >>>>>>>>>>>>
-        st.write("1) The model explains,", np.round(mt.explained_variance_score(y_test, predictions)*100,2),"% variance of the target feature")
-        
-        st.write("2) The Mean Absolute Error of model is:", np.round(mt.mean_absolute_error(predictions,y_test  ),2))
-        
-        st.write("3) MSE: ", np.round(mt.mean_squared_error(predictions,y_test ),2))
-        
-        st.write("4) The R-Square score of the model is " , np.round(mt.r2_score(predictions, y_test),2))
 
-# Define a function to perform linear regression prediction
-def prediction(target_choice, train_size, new_df, output_multi):
+    # Hide the Streamlit header and footer
+    def hide_header_footer():
+        hide_streamlit_style = """
+                    <style>
+                    footer {visibility: hidden;}
+                    </style>
+                    """
+        st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+    # increases the width of the text and tables/figures
+    _max_width_()
+
+    # hide the footer
+    hide_header_footer()
+
+image_nyu = Image.open('images/nyu.png')
+st.image(image_nyu, width=100)
+
+st.title("Food Delivery Time 🍜")
+
+# navigation dropdown
+
+st.sidebar.header("Dashboard")
+st.sidebar.markdown("---")
+app_mode = st.sidebar.selectbox('🔎 Select Page',['Introduction','Visualization','Prediction'])
+select_dataset =  st.sidebar.selectbox('💾 Select Dataset',["Delivery Time"])
+df = pd.read_csv("deliverytime.csv")
+
+
+list_variables = df.columns
+select_variable =  st.sidebar.selectbox('🎯 Select Variable to Predict',list_variables)
+# page 1 
+if app_mode == 'Introduction':
+    image_header = Image.open('./images/dataset-cover.jpg')
+    st.image(image_header, width=600)
+
+
+    st.markdown("### 00 - Show  Dataset")
+    col1, col2, col3, col4, col5, col6, col7, col8, col9, col10 = st.columns(10)
+    
+    # Descriptions for each variable in the dataset
+    # ... [The code here provides descriptions for each wine quality variable]
+    
+    # Allow users to view either the top or bottom rows of the dataset
+    num = st.number_input('No. of Rows', 5, 10)
+    head = st.radio('View from top (head) or bottom (tail)', ('Head', 'Tail'))
+    if head == 'Head':
+        st.dataframe(df.head(num))
+    else:
+        st.dataframe(df.tail(num))
+    
+    # Display the shape (number of rows and columns) of the dataset
+    st.markdown("Number of rows and columns helps us to determine how large the dataset is.")
+    st.text('(Rows,Columns)')
+    st.write(df.shape)
+    
+    st.markdown("### 01 - Description")
+    st.dataframe(df.describe())
+
+
+
+    st.markdown("### 02 - Missing Values")
+    st.markdown("Missing values are known as null or NaN values. Missing data tends to **introduce bias that leads to misleading results.**")
+    dfnull = df.isnull().sum()/len(df)*100
+    totalmiss = dfnull.sum().round(2)
+    st.write("Percentage of total missing values:",totalmiss)
+    st.write(dfnull)
+    if totalmiss <= 30:
+        st.success("Looks good! as we have less then 30 percent of missing values.")
+    else:
+        st.warning("Poor data quality due to greater than 30 percent of missing value.")
+        st.markdown(" > Theoretically, 25 to 30 percent is the maximum missing values are allowed, there's no hard and fast rule to decide this threshold. It can vary from problem to problem.")
+    
+    st.markdown("### 03 - Completeness")
+    st.markdown(" Completeness is defined as the ratio of non-missing values to total records in dataset.")
+    # st.write("Total data length:", len(df))
+    nonmissing = (df.notnull().sum().round(2))
+    completeness= round(sum(nonmissing)/len(df),2)
+    st.write("Completeness ratio:",completeness)
+    st.write(nonmissing)
+    if completeness >= 0.80:
+        st.success("Looks good! as we have completeness ratio greater than 0.85.")
+    
+    else:
+        st.success("Poor data quality due to low completeness ratio( less than 0.85).")
+
+    st.markdown("### 04 - Complete Report")
+    if st.button("Generate Report"):
+
+        pr = df.profile_report()
+        st_profile_report(pr)
+
+
+if app_mode == 'Visualization':
+    st.markdown("## Visualization")
+    symbols = st.multiselect("Select two variables",list_variables,["Type_of_vehicle", "Time_taken(min)"])
+    width1 = st.sidebar.slider("plot width", 1, 25, 10)
+    #symbols = st.multiselect("", list_variables, list_variables[:5])
+    tab1, tab2= st.tabs(["Line Chart","📈 Correlation"])    
+
+    tab1.subheader("Line Chart")
+    st.line_chart(data=df, x=symbols[0],y=symbols[1], width=0, height=0, use_container_width=True)
+    st.write(" ")
+    st.bar_chart(data=df, x=symbols[0], y=symbols[1], use_container_width=True)
+
+    #tab2.subheader("Correlation Tab 📉")
+    #fig,ax = plt.subplots(figsize=(width1, width1))
+    #sns.heatmap(df.corr(),cmap= sns.cubehelix_palette(8),annot = True, ax=ax)
+    #tab2.write(fig)
+
+
+    st.write(" ")
+    st.write(" ")
+    st.markdown("### Pairplot")
+
+    df2 = df[[list_variables[0],list_variables[1],list_variables[2],list_variables[3],list_variables[4]]]
+    fig3 = sns.pairplot(df2)
+    st.pyplot(fig3)
+
+
+
+
+if app_mode == 'Prediction':
+    st.markdown("## Prediction")
+    train_size = st.sidebar.number_input("Train Set Size", min_value=0.00, step=0.01, max_value=1.00, value=0.70)
+    new_df= df.drop(labels=select_variable, axis=1)  #axis=1 means we drop data by columns
+    list_var = new_df.columns
+    output_multi = st.multiselect("Select Explanatory Variables", list_var)
+
+    def predict(target_choice,train_size,new_df,output_multi):
+        #independent variables / explanatory variables
+        #choosing column for target
+        new_df2 = new_df[output_multi]
+        x =  new_df2
+        y = df[target_choice]
+        col1,col2 = st.columns(2)
+        col1.subheader("Feature Columns top 25")
+        col1.write(x.head(25))
+        col2.subheader("Target Column top 25")
+        col2.write(y.head(25))
+        X_train, X_test, y_train, y_test = train_test_split(x,y,test_size=train_size)
+        lm = LinearRegression()
+        lm.fit(X_train,y_train)
+        predictions = lm.predict(X_test)
+
+        return X_train, X_test, y_train, y_test, predictions,x,y
+
+    X_train, X_test, y_train, y_test, predictions,x,y= predict(select_variable,train_size,new_df,list_var)
+
+    st.subheader('🎯 Results')
+
+
+    st.write("1) The model explains,", np.round(mt.explained_variance_score(y_test, predictions)*100,2),"% variance of the target feature")
+    st.write("2) The Mean Absolute Error of model is:", np.round(mt.mean_absolute_error(y_test, predictions ),2))
+    st.write("3) MSE: ", np.round(mt.mean_squared_error(y_test, predictions),2))
+    st.write("4) The R-Square score of the model is " , np.round(mt.r2_score(y_test, predictions),2))
+
+
+
+
+if __name__=='__main__':
+    main()
+
+st.markdown(" ")
+st.markdown("### 👨🏼‍💻 **App Contributors:** ")
+st.image(['images/gaetan.png'], width=100,caption=["Gaëtan Brison"])
+
+st.markdown(f"####  Link to Project Website [here]({'https://github.com/NYU-DS-4-Everyone/Linear-Regression-App'}) 🚀 ")
+st.markdown(f"####  Feel free to contribute to the app and give a ⭐️")
+
+
+def image(src_as_string, **style):
+    return img(src=src_as_string, style=styles(**style))
+
+
+def link(link, text, **style):
+    return a(_href=link, _target="_blank", style=styles(**style))(text)
+
+
+def layout(*args):
+
+    style = """
+    <style>
+      # MainMenu {visibility: hidden;}
+      footer {visibility: hidden;background - color: white}
+     .stApp { bottom: 80px; }
+    </style>
     """
-    This function performs linear regression prediction.
+    style_div = styles(
+        position="fixed",
+        left=0,
+        bottom=0,
+        margin=px(0, 0, 0, 0),
+        width=percent(100),
+        color="black",
+        text_align="center",
+        height="auto",
+        opacity=1,
 
-    Parameters:
-    - target_choice: The target variable to be predicted.
-    - train_size: The proportion of the dataset to include in the training set.
-    - new_df: The dataframe without the target variable.
-    - output_multi: The explanatory variables selected by the user.
+    )
 
-    Returns:
-    - X_train, X_test: Training and testing data.
-    - y_train, y_test: Training and testing target values.
-    - predictions: Predicted values for the test set.
-    - x, y: Full dataset split into explanatory variables and target variable.
-    """
+    style_hr = styles(
+        display="block",
+        margin=px(8, 8, "auto", "auto"),
+        border_style="inset",
+        border_width=px(2)
+    )
 
-    # Select the explanatory variables based on user input
-    new_df2 = new_df[output_multi]
-    x = new_df2
-    y = df[target_choice]
+    body = p()
+    foot = div(
+        style=style_div
+    )(
+        hr(
+            style=style_hr
+        ),
+        body
+    )
 
-    # Display the top 25 rows of the explanatory and target variables in the Streamlit app
-    col1, col2 = st.columns(2)
-    col1.subheader("Feature Columns top 25")
-    col1.write(x.head(25))
-    col2.subheader("Target Column top 25")
-    col2.write(y.head(25))
+    st.markdown(style, unsafe_allow_html=True)
 
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=123)
+    for arg in args:
+        if isinstance(arg, str):
+            body(arg)
 
-    # Initialize and train a linear regression model
-    lm = LinearRegression()
-    lm.fit(X_train,y_train)
+        elif isinstance(arg, HtmlElement):
+            body(arg)
 
-    # Predict the target variable for the test set
-    predictions = lm.predict(X_test)
+    st.markdown(str(foot), unsafe_allow_html=True)
 
-    return X_train, X_test, y_train, y_test, predictions, x, y
+def footer2():
+    myargs = [
+        "👨🏼‍💻 Made by ",
+        link("https://github.com/NYU-DS-4-Everyone", "NYU - Professor Gaëtan Brison"),
+        "🚀"
+    ]
+    layout(*myargs)
 
 
+if __name__ == "__main__":
+    footer2()
