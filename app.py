2b515505fabb20cb81b265104d49412c961eca84
sayuh07 <145723515+sayuh07@users.noreply.github.com>
2023-10-24 17:13:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def main():
         
         st.pyplot(fig1)
 
-        st.map(df, 'Restaurant_latitude', 'Restaurant_longitude')
+        st.map(df, latitude='Restaurant_latitude', longitude='Restaurant_longitude')
         
         
         #tab2.subheader("Correlation Tab 📉")
